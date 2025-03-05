bb708c2309235682eeef500fa691c6fea0cfc931
Samar sinha <ss8987120@gmail.com>
2025-03-05 13:44:47 +0530

    version 2

---
+++ sorting/sorting.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void selection_sort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        int min = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[min]) min = j;
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+void bubble_sort(int arr[], int n){
+    for(int i=n-1; i>=0; i--){
+        int didswap = 0;
+        for(int j=0; j<i; j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp; 
+                didswap = 1;
+            }
+        }
+        if(didswap==0) break;
+    }
+}
+int main(){
+    int n;
+    printf("Enter the size of the array : ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements : \n");
+    for(int i=0; i<n; i++) scanf("%d",&arr[i]);
+    bubble_sort(arr, n);
+    for(int i=0; i<n; i++) printf("%d ",arr[i]);
+    return 0;
+}
